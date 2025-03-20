5f472aaebd2290080d9a7e8898cded9e1a943ec5
Suresh S P <suresh.sp@mathco.com>
2025-03-20 14:02:45 +0530

    added new unction

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 def function(name):
-    return f"hello{name}"
+    return f"hello {name}"
 
 function("Suresh")
 
 ## returns the name of the function
+
+def dictionary(a,b):
+    return {a:b}
+
+dictionary(4,3)
